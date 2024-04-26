36d37b2e8a1f59c2d46301a8584b6052b45eb9de
Vitor Roberto <168088088+VitorSM03@users.noreply.github.com>
2024-04-26 10:12:59 -0300

    main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def multiply(x, y):
 
 def divide(x, y):
     if y == 0:
-        return "Error! Division by zero."
+        return " Error! Division by zero. "
     else:
         return x / y
 
@@ -24,21 +24,23 @@ print(" 3 -> Multiply ")
 print(" 4 -> Divide ")
 
 while True:
+    
+    print("---------------------")
     choice = input(" Enter choice (1/2/3/4): ")
 
     if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+        num1 = float(input(" Enter first number: "))
+        num2 = float(input(" Enter second number: "))
 
         if choice == '1':
-            print("Result:", add(num1, num2))
+            print(" Result:", add(num1, num2))
         elif choice == '2':
-            print("Result:", subtract(num1, num2))
+            print(" Result:", subtract(num1, num2))
         elif choice == '3':
-            print("Result:", multiply(num1, num2))
+            print(" Result:", multiply(num1, num2))
         elif choice == '4':
-            print("Result:", divide(num1, num2))
-        repeat = input("Do you want to make another operation? (yes/no): ")
+            print(" Result:", divide(num1, num2))
+        repeat = input(" Do you want to make another operation? (yes/no): ")
         if repeat.lower() != 'yes':
             break
     else:
